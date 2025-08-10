c43dde2c85b45a347b58bbe80ffc1185b3f5e77d
Deepia <kumardeepak.parsa@gmail.com>
2025-08-10 17:47:03 +0530

    add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 function App() {
   return (
-    <div>
-      <header>
-        <a href="">Amazona</a>
-      </header>
-      <main>List of products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
